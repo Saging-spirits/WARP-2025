cf710155c493e673f6c97d8ff99fefc73ba11121
DPH75 <david_hodges75@hotmail.com>
2025-08-12 15:08:14 +0800

    Base Working

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -6,6 +6,7 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -13,27 +14,39 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class Elevator extends SubsystemBase {
 private final SparkMax Motor = new SparkMax(2, MotorType.kBrushless);
 public final RelativeEncoder encoder = Motor.getEncoder();
-public final PIDController Loop1 = new PIDController(0.3, 0, 0);
+public final PIDController Loop1 = new PIDController(1.5, 0, 0); //PID GAINS
 public boolean goingForTransfer = false;
+private final DigitalInput atPosition = new DigitalInput(2);
 
+public Trigger atPositionTrigger() {
+    return new Trigger(() -> atPosition.get());
+}
 
 public Trigger atSetpoint() { 
     return new Trigger(() -> Loop1.atSetpoint());
 }
 public Elevator() {
-    Loop1.setTolerance(1.3);
+    Loop1.setTolerance(0.25);
 }
 
 @Override
 public void periodic() {
     NetworkTableInstance.getDefault().getEntry("Elevator Setpoint").setBoolean(atSetpoint().getAsBoolean());
+    NetworkTableInstance.getDefault().getEntry("Elevator Position").setNumber(encoder.getPosition());
+}
+
+public Command runUp() {
+    return run(() -> {
+        goingForTransfer = true;
+        Motor.setVoltage(2);
+    });
 }
 
 public Command GotoPos(double position, boolean _goingForTransfer) {
     return run(()->{
         double output = Loop1.calculate(encoder.getPosition(), position);
-        double max = Math.max(output, -6);
-        double min = Math.min(max, 6);
+        double max = Math.max(output, -2);  //MAX AND MIN VOLTS
+        double min = Math.min(max, 2);
         Motor.setVoltage(min);
 
         goingForTransfer = _goingForTransfer;
@@ -45,13 +58,15 @@ public Command GotoPos(double position, boolean _goingForTransfer) {
 public Command GotoPosSlow(double position, boolean _goingForTransfer) {
     return run(()->{
         double output = Loop1.calculate(encoder.getPosition(), position);
-        double max = Math.max(output, -3);
-        double min = Math.min(max, 3);
+        double max = Math.max(output, -1); // DO NOT TOUCH!!
+        double min = Math.min(max, 1);
         Motor.setVoltage(min);
 
         goingForTransfer = _goingForTransfer;
 
  NetworkTableInstance.getDefault().getEntry("Elevator Error").setNumber(Loop1.getError());
+ NetworkTableInstance.getDefault().getEntry("Elevator Output").setNumber(min);
+
 
     });
 }
